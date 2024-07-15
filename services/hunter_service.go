1559e0b025d1ae92a599988132dfd76b342e283f
chamodshehanka <hcsperera@gmail.com>
2024-07-15 15:24:51 +0530

    Chore: Hunter service with delete

--- services/hunter_service.go
+++ services/hunter_service.go
@@ -1,40 +1,51 @@
 package services
 
 import (
-	"fmt"
 	"github.com/aws/aws-sdk-go/service/ecr"
 	"github.com/chamodshehanka/ecr-variant-hunter/internal/config"
 	"github.com/sirupsen/logrus"
+	"sync"
 	"time"
 )
 
 func DeleteOutdatedImages() {
-	//var wg sync.WaitGroup
+	var wg sync.WaitGroup
 
-	// Example: List all repositories
-	for _, repo := range config.EnvValues.RepositoryList {
-
-		logrus.Infof("Deleting old images from repository %s", repo)
-		//wg.Add(1)
-		//
-		//go func(repoName *string) {
-		//	//defer wg.Done()
-		//	deleteOldImages(svc, repoName)
-		//}(&repo)
+	ecrService, err := GetECRService()
+	if err != nil {
+		logrus.Fatalf("Error creating ECR service: %s", err)
+		return
 	}
+
+	for _, repo := range config.EnvValues.RepositoryList {
+		logrus.Infof("Deleting old images from repository %s", repo)
+		wg.Add(1)
+
+		repoCopy := repo // Create a copy of repo for the goroutine
+		go func(repoName string) {
+			defer wg.Done()
+			deleteOldImages(ecrService, &repoName)
+		}(repoCopy)
+	}
+
+	wg.Wait() // Wait for all goroutines to finish
 }
 
 func deleteOldImages(svc *ecr.ECR, repoName *string) {
 	logrus.Infof("Deleting old images from repository %s", *repoName)
 	now := time.Now()
-	cutoffDate := now.AddDate(0, 0, -14) // 14 days ago
 
-	// List images
+	imageRetentionDays := config.EnvValues.ImagesRetentionDays
+	if imageRetentionDays == 0 {
+		imageRetentionDays = 14 // Set default value to 14 if not specified
+	}
+	cutoffDate := now.AddDate(0, 0, -imageRetentionDays)
+
 	listResp, err := svc.ListImages(&ecr.ListImagesInput{
 		RepositoryName: repoName,
 	})
 	if err != nil {
-		fmt.Printf("Error listing images for repository %s: %s\n", *repoName, err)
+		logrus.Errorf("Error listing images for repository %s: %s\n", *repoName, err)
 		return
 	}
 
@@ -45,7 +56,7 @@ func deleteOldImages(svc *ecr.ECR, repoName *string) {
 			ImageIds:       []*ecr.ImageIdentifier{imageId},
 		})
 		if err != nil {
-			fmt.Printf("Error describing image %s in repository %s: %s\n", *imageId.ImageDigest, *repoName, err)
+			logrus.Errorf("Error describing image %s in repository %s: %s\n", *imageId.ImageDigest, *repoName, err)
 			continue
 		}
 
@@ -64,9 +75,9 @@ func deleteOldImages(svc *ecr.ECR, repoName *string) {
 			ImageIds:       oldImageIds,
 		})
 		if err != nil {
-			fmt.Printf("Error deleting images from repository %s: %s\n", *repoName, err)
+			logrus.Errorf("Error deleting images from repository %s: %s\n", *repoName, err)
 			return
 		}
-		fmt.Printf("Deleted %d images from repository %s\n", len(oldImageIds), *repoName)
+		logrus.Infof("Deleted %d images from repository %s\n", len(oldImageIds), *repoName)
 	}
 }
