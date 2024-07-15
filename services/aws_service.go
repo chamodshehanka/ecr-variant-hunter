b26043256aef185729951259cc098d64e233dbb0
chamodshehanka <hcsperera@gmail.com>
2024-07-15 18:56:45 +0530

    Chore: AWS Credentials

--- services/aws_service.go
+++ services/aws_service.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ecr"
 	"github.com/chamodshehanka/ecr-variant-hunter/internal/config"
@@ -10,6 +11,11 @@ import (
 func GetECRService() (*ecr.ECR, error) {
 	sess, err := session.NewSession(&aws.Config{
 		Region: &config.EnvValues.AWS.Region,
+		Credentials: credentials.NewStaticCredentials(
+			config.EnvValues.AWS.AccessKeyID,
+			config.EnvValues.AWS.SecretAccessKey,
+			"",
+		),
 	})
 
 	if err != nil {
